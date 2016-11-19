3b25c09c41b27849b0854fbdb082e36a16095493
George Brandon Dunson <georgedunson@my.unt.edu>
2016-11-19 08:56:44 -0600

    screen rotation and font size adjust for 1st prototype

--- gondola_MFr/gondola_MFr.ino
+++ gondola_MFr/gondola_MFr.ino
@@ -6,7 +6,7 @@
 ///////////////////////// display bits ////////////////////
 #include <U8g2lib.h>
 #include <Wire.h>
-U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, 2,   0,   U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R1, 2,   0,   U8X8_PIN_NONE);
                                       //(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 
 ///////////////////////// mpu6050 bits ////////////////////
@@ -47,11 +47,15 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   u8g2.begin();
 
+
   //// OLED ////
   u8g2.firstPage();
   do {
-    u8g2.setFont(u8g2_font_ncenB14_tr);
-    u8g2.drawStr(0,24,"gondola-MFr");
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+    u8g2.drawStr(5,15,"gocupi");
+    u8g2.drawStr(0,35,"gondola");
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr(2,55,"DISPLAY");
   } while ( u8g2.nextPage() );  
 
 }
